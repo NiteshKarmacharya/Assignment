72fe0456852eabd70fb5ceec0596a25cefa3f1ba
Nitesh Karmacharya <karmacharyanitesh@gmail.com>
2020-01-14 13:11:32 +0545

    Circle shape added to the code

---
+++ Assignment/circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class circle:Shape
+    {
+        int radius;
+
+        public override void set(Color colour, params int[] list)
+        {
+            //list[0] is x, list[1] is y, list[2] is width, list[3] is height
+            base.set(colour, list[0]);
+            this.radius = list[1];
+
+        }
+
+        public override void draw(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
