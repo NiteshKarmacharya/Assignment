97290e7f2c1aa9d64b17325cbc86d8af456efd50
Nitesh Karmacharya <karmacharyanitesh@gmail.com>
2020-01-14 13:17:23 +0545

    Line Shape is added to the code

---
+++ Assignment/line.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class line: Shape
+    {
+        int p1,p2;
+
+        public override void set(Color colour, params int[] list)
+        {
+            //list[0] is x, list[1] is y, list[2] is width, list[3] is height
+            base.set(colour, list[0], list[1]);
+            this.p1 = list[2];
+            this.p2 = list[3];
+
+        }
+
+        public override void draw(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
